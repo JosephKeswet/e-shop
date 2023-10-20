5552b3e1ab7b436bc61b56fcdf934d673e52eac3
JosephKeswet <josephkeswet@gmail.com>
2023-10-20 08:50:22 +0100

    feat:enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin';
 
-const serviceAccount = require("../serviceAccountKey.json");
-
+const serviceAccount = require('../serviceAccountKey.json');
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
